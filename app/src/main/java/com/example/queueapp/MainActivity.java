f0f86e0c6bd7a11ecd67b6dadcac770a140d8b58
Lok <you@example.com>
2021-02-22 06:58:36 +0800

    error fixed

--- app/src/main/java/com/example/queueapp/MainActivity.java
+++ app/src/main/java/com/example/queueapp/MainActivity.java
@@ -7,7 +7,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
-import android.provider.MediaStore;
+
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -69,12 +69,6 @@ public class MainActivity extends AppCompatActivity {
                     String restaurantId = edtTxtRestaurantId.getText().toString();
                     restaurantPage.actionStart(MainActivity.this, restaurantId);
                 }
-            }
-        });
-        cam.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
             }
         });
     }
